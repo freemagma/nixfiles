3c531a93619e8ed6a6c9903afcd636d30718eb6a
Charles Gunn <cjgunnmail@gmail.com>
2021-09-08 15:12:04 -0400

    circuitsim

---
+++ hcom/pkgs/circuitsim/default.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> { }, ... }:
+with pkgs;
+
+let
+  pname = "CircuitSim";
+  version = "1.8.2-2110";
+  schemaPath = "${gtk3}/share/gsettings-schemas/gtk+3-${gtk3.version}";
+in stdenv.mkDerivation (rec {
+  inherit pname version;
+
+  buildInputs = [ makeWrapper ];
+
+  unpackPhase = "true";
+  buildPhase = "true";
+
+  installPhase = ''
+    mkdir -p "$out/bin" "$out/share/java"
+    cp ${./csim.jar} "$out/share/java/CircuitSim.jar"
+    makeWrapper \
+      "${openjdk11}/bin/java" \
+      "$out/bin/${pname}" \
+      --prefix XDG_DATA_DIRS : "${schemaPath}" \
+      --add-flags "-jar $out/share/java/CircuitSim.jar" '';
+})
