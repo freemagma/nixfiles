2f727322244079f3ac4e4caf0244da0eef8264e2
Charles Gunn <me@cjgunn.com>
2021-10-08 22:37:10 -0400

    removed non-flake stuff

--- setup.sh
+++
@@ -1,8 +0,0 @@
-if [ -d "$1" ]; then
-    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-    sudo mv "/etc/nixos" "/etc/nixos.bak"
-    sudo ln -s "$SCRIPT_DIR" "/etc/nixos"
-    echo "{ imports = [ ./$1 ]; }" >> "$SCRIPT_DIR/configuration.nix"
-else 
-    echo "Please enter a valid hostname as input" >&2; exit 1;
-fi
