2bbc0f9239a188ecc7789997cd1568870e9286de
Charles Gunn <me@cjgunn.com>
2021-12-01 23:12:55 -0500

    idk

--- hcom/doom-emacs/doom/config.el
+++ hcom/doom-emacs/doom/config.el
@@ -1,6 +1,7 @@
 (setq +latex-viewers '(pdf-tools))
 ;; (add-to-list 'warning-suppress-types '(undo discard-info))
 (setq doom-theme 'doom-gruvbox)
+(setq display-line-numbers-type 'relative)
 
 (map! :after python
       :map python-mode-map
