c81f2f8a7abca91f8e11ec4e099b73b193816258
Charles Gunn <me@cjgunn.com>
2022-02-04 13:07:42 -0500

    latex treesitter sad

--- hcom/neovim/treesitter.lua
+++ hcom/neovim/treesitter.lua
@@ -1,7 +1,6 @@
 local treesitter = require("nvim-treesitter.configs")
 treesitter.setup {
-  ensure_installed = "maintained",
-  highlight = {
-    enable = true
-  }
+    ensure_installed = "maintained",
+    ignore_install = {"lua", "latex"},
+    highlight = {enable = true}
 }
