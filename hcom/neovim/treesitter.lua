92ab07d3f2f594ed1cfe4c83b435e80daaa74be2
Charles Gunn <me@cjgunn.com>
2022-02-09 12:52:03 -0500

    dont ignore lua and latex

--- hcom/neovim/treesitter.lua
+++ hcom/neovim/treesitter.lua
@@ -1,6 +1,5 @@
 local treesitter = require("nvim-treesitter.configs")
 treesitter.setup {
     ensure_installed = "maintained",
-    ignore_install = {"lua", "latex"},
     highlight = {enable = true}
 }
