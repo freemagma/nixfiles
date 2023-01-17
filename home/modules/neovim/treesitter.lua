46f861d121abf53c1113769add29f76b96967437
Charles Gunn <me@cjgunn.com>
2023-01-17 12:10:47 -0500

    verilog treesitter

--- home/modules/neovim/treesitter.lua
+++ home/modules/neovim/treesitter.lua
@@ -8,7 +8,7 @@ treesitter.setup {
     ensure_installed = {
         "c", "lua", "bibtex", "cmake", "cpp", "css", "dockerfile", "elixir",
         "html", "javascript", "json", "latex", "lua", "make", "nix", "ocaml",
-        "python", "scss", "toml", "typescript", "yaml", "zig"
+        "python", "scss", "toml", "typescript", "verilog", "yaml", "zig"
     },
     highlight = {enable = true}
 }
